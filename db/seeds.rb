62f9c84b9c361ac677c44da46b53a90e6378ee09
1nf1n1t3f1r3 <1nf1n1t3f1r3@gmail.com>
2026-04-09 16:31:46 +0200

    Added Posts Model
    Added Seeds
    	User + Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,44 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Clear existing data (optional, but useful while developing)
+your_email = "johne1234@gmail.com"
+
+# Keep your user, delete others
+User.where.not(email: your_email).destroy_all
+Post.destroy_all
+
+# Create additional users
+10.times do |i|
+  user = User.create!(
+    email: "user#{i + 1}@example.com",
+    username: "user#{i + 1}",
+    password: "password"
+  )
+
+  rand(2..5).times do
+    Post.create!(
+      content: [
+        "Hello world!",
+        "My first post!",
+        "Rails is fun 😄",
+        "Working on Odinbook",
+        "Coffee + coding ☕"
+      ].sample,
+      user: user
+    )
+  end
+end
+
+# Give YOUR user some posts too
+current_user = User.find_by(email: your_email)
+
+if current_user
+  3.times do |i|
+    Post.create!(
+      content: "My personal post #{i + 1}",
+      user: current_user
+    )
+  end
+end
