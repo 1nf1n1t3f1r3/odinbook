95169125e4ebb932f43c9846a8f17b0d6b19a20d
1nf1n1t3f1r3 <1nf1n1t3f1r3@gmail.com>
2026-04-09 13:27:27 +0200

    Added Devise Added Devise User 	Email/Username/PW 	Signup 	Login
    Added Header with Login/Logout
    
    Login Required to see/do anything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  # Need to Sign Up to do anything
+  before_action :authenticate_user!
+
+  # Add Usernames
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
+  end
 end
